e2769b239a4f8177a943954fa81ec7a8d8e62c25
Simon Petit <nomisp96@hotmail.fr>
2023-05-25 08:42:11 +0200

    Adding token saving

--- lib/rfc7230.ml
+++ lib/rfc7230.ml
@@ -1,178 +1,198 @@
-let rec state_1 input =
+let from_rev_char_list_to_string token =
+	String.of_seq ( List.to_seq ( List.rev token ) )
+
+let rec state_1 input token l =
 	let c = input_char input in
 	match c with
 	(* method *)
-	| 'a'..'z' | 'A'..'Z' | '-' (* tchar *) ->
-		 state_1 input 
+	| 'a'..'z' | 'A'..'Z' | '-' as x (* tchar *) ->
+		 state_1 input (x::token) l
 	(* WS *) 
 	| ' ' -> 
-		state_2 input 
-	| _ -> "error1" 
-and state_2 input = (* shall recognize the reques-target *)
+		let tok = from_rev_char_list_to_string token in
+		state_2 input [] (tok::l)
+	| _ -> "error1"::[]
+and state_2 input token l = (* shall recognize the request-target *)
 	let c = input_char input in
 	match c with
 	(* origin-form *)
-	| '/' -> 
-		state_3 input 
+	| '/' as x -> 
+		state_3 input (x::token) l
 	(* asterisk-form *) 
 	(* | '*' -> state_x input *)
-	| _ -> "error2"
-and state_3 input = (* absolute-path *)
+	| _ -> "error2"::[]
+and state_3 input token l = (* absolute-path *)
 	let c = input_char input in
 	match c with
 	(* segment *)
- 	| 'a'..'z' | 'A'..'Z' | '0'..'9' (* pchar *) -> 
-		state_3 input 
+ 	| 'a'..'z' | 'A'..'Z' | '0'..'9' (* pchar *) as x -> 
+		state_3 input (x::token) l
 	(* absolute-path *) 
-	| '/' -> state_3 input 
+	| '/' as x -> state_3 input (x::token) l
 	(* query *)
-	| '?' -> state_4 input 
+	| '?' as x -> state_4 input (x::token) l
 	(* request-target is done *)
-	| ' ' -> state_5 input 
-	| _ -> "error3"
-and state_4 input =
+	| ' ' -> 
+		let tok = from_rev_char_list_to_string token in 
+		state_5 input [] (tok::l)
+	| _ -> "error3"::[]
+and state_4 input token l =
 	let c = input_char input in
 	match c with
 	(* query is empty and request_target is done *)
-	| ' ' -> state_5 input
+	| ' ' -> 
+		let tok = from_rev_char_list_to_string token in 
+		state_5 input [] (tok::l)
 	(* query *)
-	| 'a'..'z' | 'A'..'Z' | '0'..'9' (* pchar *) ->  
-			state_4 input 
+	| 'a'..'z' | 'A'..'Z' | '0'..'9' as x (* pchar *) ->  
+			state_4 input (x::token) l
 	(* query *)
-	| '?' -> state_4 input
+	| '?' as x -> 
+		state_4 input (x::token) l
 	(* query *)
-	| '/' -> state_4 input
-	| _ -> "error4"
-and state_5 input =(* HTTP version *)
+	| '/' as x -> 
+		state_4 input (x::token) l
+	| _ -> "error4"::[]
+and state_5 input token l =(* HTTP version *)
 	let c = input_char input in
 	match c with
-	| 'H' -> state_6 input 
-	| _ -> "error5"
-and state_6 input = 
+	| 'H' -> state_6 input token l
+	| _ -> "error5"::[]
+and state_6 input token l = 
 	let c = input_char input in
 	match c with
-	| 'T' -> state_7 input
-	| _ -> "error6"
-and state_7 input =
+	| 'T' -> state_7 input token l
+	| _ -> "error6"::[]
+and state_7 input token l=
 	let c = input_char input in
 	match c with
-	| 'T' -> state_8 input
-	| _ -> "error7"
+	| 'T' -> state_8 input token l
+	| _ -> "error7"::[]
 				
-and state_8 input =
+and state_8 input token l =
 	let c = input_char input in
 	match c with
-	| 'P' -> state_9 input 
-	| _ -> "error8"
-and state_9 input =
+	| 'P' -> state_9 input token l
+	| _ -> "error8"::[]
+and state_9 input token l =
 	let c = input_char input in
 	match c with
-	| '/' -> state_10 input
-	| _ -> "error9"
-and state_10 input =
+	| '/' -> state_10 input token l 
+	| _ -> "error9"::[]
+and state_10 input token l =
 	let c = input_char input in
 	match c with
-	| '0'..'9' -> state_11 input
-	| _ -> "error10"	
-and state_11 input =
+	| '0'..'9' -> state_11 input token l
+	| _ -> "error10"::[]
+and state_11 input token l =
 	let c = input_char input in
 	match c with
-	| '.' -> state_12 input 
-	| _ -> "error11"
+	| '.' -> state_12 input token l
+	| _ -> "error11"::[]
 
-and state_12 input =
+and state_12 input token l =
 	let c = input_char input in
 	match c with
-	| '0'..'9' -> state_13 input (* HTTP version is done *)
-	| _ -> "error12"
+	| '0'..'9' -> state_13 input token l(* HTTP version is done *)
+	| _ -> "error12"::[]
 
-and state_13 input =
+and state_13 input token l =
 	let c = input_char input in
 	match c with
-	| '\r' -> state_14 input (* Beginning the first CRLF *)
-	| _ -> "error13" 
+	| '\r' -> state_14 input token l (* Beginning the first CRLF *)
+	| _ -> "error13"::[]
 	
-and state_14 input =
+and state_14 input token l =
 	let c = input_char input in
 	match c with
-	| '\n' -> state_15 input (* end of first line CRLF done *)
-	| _ -> "error14" 
+	| '\n' -> state_15 input token l (* end of first line CRLF done *)
+	| _ -> "error14"::[]
 
-and state_15 input =
+and state_15 input token l =
 	let c = input_char input in
 	match c with
-	| '\r' -> state_16 input (* second CRLF, no headers *)
-	| 'a'..'z' | 'A'..'Z' -> state_18 input (* token of the fieldname *)
-	| _ -> "error15"
+	| '\r' -> state_16 input l (* second CRLF, no headers *)
+	| 'a'..'z' | 'A'..'Z' as x -> 
+		state_18 input (x::token) l (* token of the fieldname *)
+	| _ -> "error15"::[]
 
-and state_16 input =
+and state_16 input l =
 	let c = input_char input in
 	match c with
 	(* end of header *)
 	(* | '\n' -> State_17 input *)
-	| '\n' -> "ok"
-	| _ -> "error16"
-and state_18 input =
+	| '\n' -> l
+	| _ -> "error16"::[]
+and state_18 input token l =
 	let c = input_char input in
 	match c with
-	| 'a'..'z' | 'A'..'Z' (* tchar *) -> state_18 input (* fieldname *)
+	| 'a'..'z' | 'A'..'Z' (* tchar *) as x -> 
+		 state_18 input (x::token) l(* fieldname *)
 	(* end of fieldname *)
-	| ':' -> state_19 input 
-	| _ -> "error18"
+	| ':' -> 
+		let tok = from_rev_char_list_to_string token in 
+		state_19 input [] (tok::l)
+	| _ -> "error18"::[]
 
-and state_19 input =
+and state_19 input token l =
 	let c = input_char input in
 	match c with
-	| ' ' -> state_20 input (* OWS *)
-	| '\t' -> state_20 input (* OWS *) 
-	| 'a'..'z' | 'A'..'Z' | '0'..'9' (*vchar*) -> state_21 input (* beginning fieldvalue *)
+	| ' ' -> state_20 input token l (* OWS *)
+	| '\t' -> state_20 input token l (* OWS *) 
+	| 'a'..'z' | 'A'..'Z' | '0'..'9' (*vchar*) as x -> 
+		state_21 input (x::token) l (* beginning fieldvalue *)
 	(* end of OWS and no fieldvalue *)
-	| '\r' -> state_22 input 
-	| _ -> "error19"
+	| '\r' -> state_22 input token l
+	| _ -> "error19"::[]
 
-and state_20 input =
+and state_20 input token l =
 	let c = input_char input in
 	match c with
-	| ' ' -> state_20 input (* OWS *)
-	| '\t' -> state_20 input (* OWS *)
-	| 'a'..'z' | 'A'..'Z' | '0'..'9' (*vchar*) -> state_21 input (* beginning fieldvalue *)
-	| _ -> "error20"
+	| ' ' -> state_20 input token l (* OWS *)
+	| '\t' -> state_20 input token l (* OWS *)
+	| 'a'..'z' | 'A'..'Z' | '0'..'9' (*vchar*) as x -> 
+		 state_21 input (x::token) l (* beginning fieldvalue *)
+	| _ -> "error20"::[]
 
 
-and state_21 input =
+and state_21 input token l =
 	let c = input_char input in
 	match c with
-	| 'a'..'z' | 'A'..'Z' | '0'..'9' (*vchar*) -> state_21 input
-	| ' ' -> state_21 input
-	| '\t' -> state_21 input
-	| '\r' -> state_22 input 
-	| _ -> "error21"
+	| 'a'..'z' | 'A'..'Z' | '0'..'9' (*vchar*) as x -> 
+		state_21 input (x::token) l
+	| ' ' -> state_21 input token l
+	| '\t' -> state_21 input token l
+	| '\r' -> 
+		let tok = from_rev_char_list_to_string token in 
+		state_22 input [] (tok::l)
+	| _ -> "error21"::[]
 
-and state_22 input =
+and state_22 input token l =
 	let c = input_char input in
 	match c with
-	| '\n' -> state_23 input
+	| '\n' -> state_23 input token l
 	(* | '\n' -> "ok" *)
-	| _ -> "error22"
+	| _ -> "error22"::[]
 	
 
-and state_23 input =
+and state_23 input token l =
 	let c = input_char input in
 	match c with
-	| 'a'..'z' | 'A'..'Z' (* tchar *) -> state_18 input (* another header field *)
-	| '\r' -> state_24 input
-	| _ -> "error23"
+	| 'a'..'z' | 'A'..'Z' as x (* tchar *) -> 
+		 state_18 input (x::token) l (* another header field *)
+	| '\r' -> state_24 input l
+	| _ -> "error23"::[]
 
-and state_24 input =
+and state_24 input  l =
 	let c = input_char input in
 	match c with
 	(* | '\n' -> State_17 input *)
-	| '\n' -> "OK"
-	| _ -> "error24"
+	| '\n' -> l
+	| _ -> "error24"::[]
 
 
 let parse input =
-	state_1 input 
+	state_1 input [] []
 
 	
 (*
--- test/test_solidaml.ml
+++ test/test_solidaml.ml
@@ -1,8 +1,14 @@
 open Solidaml
 
+let rec   print_list l = 
+  match l with 
+  | [] -> Printf.printf "\n"
+  | x::xs -> Printf.printf "%s\n" x; print_list xs
+
 let () = 
   let filename = "test" in 
   let req = open_in filename in 
   let res = Rfc7230.parse req in 
-  let () = Printf.printf "%s\n" res in 
+  (* let () = Printf.printf "%s\n" res in  *)
+  print_list (List.rev res);
   close_in req
